947c9e56a20d8fd9e1dee5b8dad9fe342bdf9197
samergain <samersadon@gmail.com>
2022-05-02 16:54:04 -0700

    select all, select single or multiple values, and orderby

--- LINQintro/ManageEmployee.cs
+++ LINQintro/ManageEmployee.cs
@@ -10,29 +10,65 @@ namespace LINQintro
     {
         EmployeeRepository employeeRepository = new EmployeeRepository();
 
-        private Employee Demo(Employee emp)
-        {
-            return emp;
-        }
-
         //SELECT 
+        
         private void SelectDemo()
         {
             //select * from employees
             //query syntax
-            var result = from employee in employeeRepository.GetEmployees()
-                         select employee;
-            foreach(var employee in result)
-            {
-                Console.WriteLine(employee.Id + "\t" + employee.FullName + "\t" + employee.Department + "\t" + employee.Salary + "\t" + employee.Age);
-            }
+            //var result = from employee in employeeRepository.GetEmployees()
+            //           select employee;
 
+            //method syntax
+            //var result = employeeRepository.GetEmployees().Select(employee => employee);
+
+            //foreach (var employee in result)
+            //{
+            //    Console.WriteLine(employee.Id + "\t" + employee.FullName + "\t" + employee.Department + "\t" + employee.Salary + "\t" + employee.Age);
+            //}
+
+            //Single Value
+            //query syntax
+            //var result = from employee in employeeRepository.GetEmployees()
+            //             select employee.FullName;
+
+            //method syntax
+            //var result = employeeRepository.GetEmployees().Select(emp => emp.FullName);
+            //foreach (var employee in result)
+            //{
+            //    Console.WriteLine(employee);
+            //}
+
+            //Multiple values
+            //query syntax
+            //var result = (from employee in employeeRepository.GetEmployees()
+            //             select employee.Department).Distinct();
+            var result = employeeRepository.GetEmployees().Select(emp => new { Name = emp.FullName, Department = emp.Department })
+                .FirstOrDefault(x => x.Department == "ddsf") ?? new { Name = "N/A", Department = "N/A"};
+            Console.WriteLine(result);
         }
 
+        private void OrderByDemo()
+        {
+            //var result = from employee in employeeRepository.GetEmployees()
+            //             orderby employee.Salary descending, employee.FullName descending
+            //             select new
+            //             {
+            //                 Name = employee.FullName,
+            //                 Salary = employee.Salary
+            //             };
+
+            //method syntax
+            var result = employeeRepository.GetEmployees().OrderByDescending(x => x.Salary).ThenByDescending(x => x.FullName).Select(x => new { Name = x.FullName, Salary = x.Salary });
+            foreach (var x in result)
+            {
+                Console.WriteLine(x.Name+'\t'+x.Salary);
+            }
+        }
 
         public void Run()
         {
-            SelectDemo();
+            OrderByDemo();
         }
     }
 }
